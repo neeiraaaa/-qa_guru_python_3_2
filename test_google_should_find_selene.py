8eb4a2852fc861368a852e505f8d7cedbabeaf90
irina <neeiraaaa@gmail.com>
2022-11-23 18:33:35 +0300

    add negative and positive checks

--- test_google_should_find_selene.py
+++ test_google_should_find_selene.py
@@ -2,6 +2,10 @@ from selene.support.shared import browser
 from selene import be, have
 
 
-browser.open('https://google.com')
-browser.element('[name="q"]').should(be.blank).type('selene').press_enter()
-browser.element('[id="search"]').should(have.text('yashaka/selene: User-oriented Web UI browser tests in ...'))
+def test_successful_search(open_browser):
+    browser.element('[name="q"]').should(be.blank).type('selene').press_enter()
+    browser.element('[id="search"]').should(have.text('yashaka/selene: User-oriented Web UI browser tests in Python'))
+
+def test_negative_search(open_browser):
+    browser.element('[name="q"]').should(be.blank).type('fjfjfjfjjdlelcfkefldvlsbnd').press_enter()
+    browser.element('[id= "rcnt"]').should(have.text('ничего не найдено'))
